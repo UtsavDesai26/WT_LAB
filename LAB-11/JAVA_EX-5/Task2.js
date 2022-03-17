e82b64e8e13d63a2ed2dd0e2837bd7dc9919e117
UtsavDesai987 <80502799+UtsavDesai987@users.noreply.github.com>
2022-03-17 22:23:03 +0530

    Delete LAB-11/JAVA_EX-5 directory

--- LAB-11/JAVA_EX-5/Task2.js
+++
@@ -1,9 +0,0 @@
-let val = parseInt(prompt("Enter the Number="));
-let num = "";
-
-for(var i=1;i<=val;i++){
-    num += i+"<br>";
-    console.log(i);
-}
-
-document.getElementById('Number').innerHTML = num;
